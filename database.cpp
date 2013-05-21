f92906a6dd6f06f6082b87f85fb056ce3bf3e590
Christophe de Dinechin <ddd@gigo.(none)>
2013-05-21 13:01:50 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~/Work/tao

--- database.cpp
+++ database.cpp
@@ -33,6 +33,8 @@ using namespace Tao;
 
 XL_DEFINE_TRACES
 
+static LicenseTimer lic;
+
 
 
 Tree_p setConnection(Tree_p /* self */,
@@ -51,7 +53,12 @@ Tree_p setConnection(Tree_p /* self */,
                     << "\tsetUserName [" <<  setUserName->value << "]\n"
                     << "\tsetPassword [" <<  setPassword->value << "]\n"
                     << "\tsetPort [" <<  setPort->value << "]\n";
-
+    if (! lic.isConnectionAuthorized())
+    {
+        IFTRACE(dbconnector_W)
+                debug() << "WARNING: unlicensed module. Trial time as expired.\n";
+        return XL::xl_false;
+    }
     QString sgbd = +SGBD->value;
     QString HostName = +setHostName->value;
     QString DatabaseName = +setDatabaseName->value;
@@ -376,13 +383,24 @@ Tree_p relationalQuery(Tree_p self, Text_p DatabaseName,
     return res;
 
 }
-int module_init(const Tao::ModuleApi *, const Tao::ModuleInfo *)
+int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
 // ----------------------------------------------------------------------------
 //   Initialize the Tao module
 // ----------------------------------------------------------------------------
 {
     XL_INIT_TRACES();
 
+    // Module available for Impress version so silently check it before
+    // checking with potential warning window "dbConnector" license.
+    if ( ! api->hasLicense("Tao Presentations Impress 1.001") &&
+         ! api->checkLicense("dbConnector 1.0", false) )
+    {
+        lic.start(5 * 60 * 1000);
+        IFTRACE(dbconnector_W)
+                debug() << "WARNING: No license available."
+                        << " Connections available for "
+                        << lic.interval() << " msec. \n";
+    }
     fillDrivers();
 
     return no_error;
