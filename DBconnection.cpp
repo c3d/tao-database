c524d89f4e28b5b9ffd2c0f5393b3b97d81fa411
Christophe de Dinechin <christophe@taodyne.com>
2014-10-30 14:43:45 +0100

    License changes

--- DBconnection.cpp
+++ DBconnection.cpp
@@ -11,8 +11,8 @@
 //
 //
 // ****************************************************************************
-// This software is property of Taodyne SAS - Confidential
-// Ce logiciel est la propriete de Taodyne SAS - Confidentiel
+// This software is licensed under the GNU General Public License v3.
+// See file COPYING for details.
 //  (C) 2011 Taodyne SAS
 // ****************************************************************************
 #include "DBconnection.h"
